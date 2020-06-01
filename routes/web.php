86910d88ea64bd1ac609c0caa8cfe4192098a5c2
mariamyoussefwiliam <61950354+mariamyoussefwiliam@users.noreply.github.com>
2020-06-01 19:14:32 +0200

    add dashboard model controller and view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//----------------------------------------------------------------------------
+route::namespace('admin')->prefix('/dashboard')->group(function(){
+
+    Route::get('/login', 'authcontroller@login')->name('admin.login');
+    Route::post('/dologin', 'authcontroller@dologin')->name('admin.dologin');
+   
+
+Route::middleware('adminauth:admin')->group(function(){ //middleware name adminauth 3la guard el admin 
+
+     Route::get('', 'homecontroller@index')->name('admin.home'); //awl saf7a 5als
+     Route::get('/logout', 'authcontroller@logout')->name('admin.logout'); //logout
+    
+    
+    });
+
+    
+});
