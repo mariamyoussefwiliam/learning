3f30b2ed54f0e9d3494b85e7b29dcac04aaa8e29
mariamyoussefwiliam <61950354+mariamyoussefwiliam@users.noreply.github.com>
2020-06-04 02:13:00 +0200

    home page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+});*/
+route::namespace('front')->group(function(){
 
+    Route::get('/', 'homepagecontroller@index')->name('front.homepage'); 
+   
+});
 //----------------------------------------------------------------------------
 route::namespace('admin')->prefix('/dashboard')->group(function(){
 
